c4ee4579c92a7cba581986f27b058c30620f76c1
zeka <allrent3@gmail.com>
2021-05-12 21:28:47 +0300

    ere

--- src/main/java/T1.java
+++ src/main/java/T1.java
@@ -1,19 +1,28 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class T1 {
-
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Ivan", "Peter", "Dima", "Kolia", "Vasyl");
-        int count = 0;
-        for (int i = 0; i <= names.size() - 1; i++) {
-            String name = names.get(i);
-            count++;
-            name = count + ". " + name + ", ";
-            if (count % 2 != 0) {
-               System.out.print(name);
-            }
-        }
+//        int count = 0;
+//        for (int i = 0; i <= names.size() - 1; i++) {
+//            String name = names.get(i);
+//            count++;
+//            name = count + ". " + name + ", ";
+//            if (count % 2 != 0) {
+//               System.out.print(name);
+//            }
+//        }
+        String s = fromListToString(names);
+        System.out.println(s);
+    }
+    private static String fromListToString(List<String> names) {
+        return names.stream()
+                .filter(name -> names.indexOf(name) % 2 != 0)
+                .map(name ->  names.indexOf(name) + ". " + name + ", ")
+                .collect(Collectors.joining());
     }
 }
+
 
--- src/main/java/T5.java
+++ src/main/java/T5.java
@@ -1,17 +1,31 @@
-
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class T5 {
+    static Long a;
 
-    public static void main(String[] args){
-        Stream<Integer> first = Stream.of(1,2,3,4,5);
-        Stream<Integer> second = Stream.of(6,7,8,9);
-       zip(first,second).forEach(System.out::print);
+    public static void main(String[] args) {
+        List<Integer> number1 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> number2 = Arrays.asList(7, 8, 9, 0);
+
+        int q = number1.size();
+        int w = number2.size();
+        if (q > w) {
+            a = (long) w;
+        }
+        if (w > q) {
+            a = (long) q;
+        }
+        Stream<Integer> first = number1.stream();
+        Stream<Integer> second = number2.stream();
+
+        zip(first, second).forEach(System.out::print);
     }
+
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
 
-         return Stream.concat(first,second).parallel();
-
+        return Stream.concat(first.limit(a).parallel(), second.limit(a).parallel());
     }
 
 }
